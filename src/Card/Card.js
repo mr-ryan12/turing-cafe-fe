4485bfeed9e7283bd889378e6b86b12df6ae697a
Ryan McBride <mr.ryan444@gmail.com>
2022-02-16 11:43:52 -0700

    Create function to format dates

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './Card.css'
 
+const checkDates = date => {
+  const splitDate = date.split('/')
+  let formattedMonth;
+  let formattedDay;
+
+  if (splitDate[0].length === 1) {
+    formattedMonth = '0' + splitDate[0]
+  } else {
+    formattedMonth = splitDate[0];
+  }
+
+  if (splitDate[1].length === 1) {
+    formattedDay = '0' + splitDate[1]
+  } else {
+    formattedDay = splitDate[1]
+  } 
+
+  return formattedMonth + '/' + formattedDay
+}
+
 const Card = props => {
   return (
     <section className="reservation-card">
       <h2 className="card-name">{props.name}</h2>
-      <p className="card-date">{props.date}</p>
+      <p className="card-date">{checkDates(props.date)}</p>
       <p className="card-time">{props.time}</p>
       <p className="card-number">Number of Guests: {props.number}</p>
       <button className="cancel-button">Cancel</button>
